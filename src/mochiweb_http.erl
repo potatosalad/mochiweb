0c5196fa1569bfe9f95558dad98ff771a0e2b3f0
Daniel Barney <bar08023@byui.edu>
2012-02-22 11:52:42 -0700

    can now handle large http requests

--- src/mochiweb_http.erl
+++ src/mochiweb_http.erl
@@ -47,7 +47,8 @@ start_link(Options) ->
     mochiweb_socket_server:start_link(parse_options(Options)).
 
 loop(Socket, Body) ->
-    ok = mochiweb_socket:setopts(Socket, [{packet, http}]),
+    %% allow for large http packets
+    ok = mochiweb_socket:setopts(Socket, [{packet, http},{packet_size, 1024*8},{buffer, 1024*8}]),
     request(Socket, Body).
 
 -ifdef(gen_tcp_r15b_workaround).
